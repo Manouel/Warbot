fe7570624aa4fed344337040923e11dc0cadb112
maluna <manuel.cha34@gmail.com>
2014-12-07 16:41:22 +0100

    Explorer en fsm

---
+++ taches/ChercherNourriture.java
@@ -0,0 +1,98 @@
+package pepisha.taches;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import pepisha.Constants;
+import pepisha.WarExplorerBrainController;
+import edu.turtlekit3.warbot.agents.ControllableWarAgent;
+import edu.turtlekit3.warbot.agents.MovableWarAgent;
+import edu.turtlekit3.warbot.agents.enums.WarAgentType;
+import edu.turtlekit3.warbot.agents.percepts.WarPercept;
+import edu.turtlekit3.warbot.brains.WarBrainController;
+import edu.turtlekit3.warbot.communications.WarMessage;
+
+public class ChercherNourriture extends TacheAgent {
+
+	public ChercherNourriture(WarBrainController b){
+		super(b);
+	}
+	
+	
+	/**
+	 * @action Récupère un message à propos de la nourriture s'il y en a
+	 * @return Message food si trouvé, sinon null
+	 */
+	private WarMessage getMessageAboutFood()
+	{
+		WarExplorerBrainController explorer = (WarExplorerBrainController) typeAgent;
+		
+		for (WarMessage m : explorer.getBrain().getMessages())
+		{
+			if(m.getMessage().equals("foodHere"))
+				return m;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void exec() {
+		WarExplorerBrainController explorer = (WarExplorerBrainController) typeAgent;
+		
+		// Si mon sac est plein je change de tache
+		if (explorer.getBrain().isBagFull()) {
+			explorer.setTacheCourante(new RetournerNourriture(typeAgent));
+			return;
+		}
+
+		if (explorer.getBrain().isBlocked())
+			explorer.getBrain().setRandomHeading();
+			
+		ArrayList<WarPercept> nourriture = explorer.getBrain().getPerceptsResources();
+		
+		// Si il y a de la nourriture dans notre champ de vision
+		if(nourriture != null && nourriture.size() > 0)
+		{			
+			WarPercept lePlusProche = nourriture.get(0); // le 0 est le plus proche normalement
+			
+			if(lePlusProche.getDistance() <= ControllableWarAgent.MAX_DISTANCE_GIVE) {
+				// On envoie un message aux autres explorer pour dire qu'il y a de la nourriture
+				explorer.getBrain().broadcastMessageToAgentType(WarAgentType.WarExplorer, Constants.foodHere, "");
+				explorer.setToReturn(MovableWarAgent.ACTION_TAKE);
+			} else {
+				explorer.getBrain().setHeading(lePlusProche.getAngle());
+				explorer.setToReturn(MovableWarAgent.ACTION_MOVE);
+			}
+			
+			explorer.setDistance(0.0);
+		}
+		else 		// Pas de nourriture dans notre champ de vision
+		{
+			// Pas de message reçu
+			if (explorer.getDistance() <= 0.0) {
+
+				// Je regarde si j'ai reçu un message qui dit qu'il y a de la nourriture
+				WarMessage food = getMessageAboutFood();
+				
+				if (food != null) {
+					explorer.setDistance(food.getDistance());
+					explorer.getBrain().setHeading(food.getAngle());
+				}
+				else {
+					explorer.getBrain().setRandomHeading(40);
+				}
+			}
+			else {
+				explorer.setDistance(explorer.getDistance()-1);
+			}
+			
+			explorer.setToReturn(MovableWarAgent.ACTION_MOVE);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Tache Chercher Nourriture";
+	}
+}
---
+++ taches/RetournerNourriture.java
@@ -0,0 +1,81 @@
+package pepisha.taches;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import pepisha.Constants;
+import pepisha.WarExplorerBrainController;
+import edu.turtlekit3.warbot.agents.MovableWarAgent;
+import edu.turtlekit3.warbot.agents.enums.WarAgentType;
+import edu.turtlekit3.warbot.agents.percepts.WarPercept;
+import edu.turtlekit3.warbot.brains.WarBrainController;
+import edu.turtlekit3.warbot.communications.WarMessage;
+
+public class RetournerNourriture extends TacheAgent {
+	
+	public RetournerNourriture(WarBrainController b){
+		super(b);
+	}
+	
+	/**
+	 * @action Récupère les messages reçus provenants des bases
+	 */
+	private WarMessage getMessageFromBase()
+	{
+		WarExplorerBrainController explorer = (WarExplorerBrainController) typeAgent;
+		
+		for (WarMessage m : explorer.getBrain().getMessages())
+		{
+			if(m.getSenderType().equals(WarAgentType.WarBase))
+				return m;
+		}
+		
+		explorer.getBrain().broadcastMessageToAgentType(WarAgentType.WarBase, Constants.whereAreYou, "");
+		return null;
+	}
+
+	@Override
+	public void exec() {
+		WarExplorerBrainController explorer = (WarExplorerBrainController) typeAgent;
+		
+		if(explorer.getBrain().isBlocked())
+			explorer.getBrain().setRandomHeading();
+		
+		
+		// On récupère la liste des bases alliées qui sont dans les environs
+		ArrayList<WarPercept> bases = explorer.getBrain().getPerceptsAlliesByType(WarAgentType.WarBase);
+		
+		// Si pas de base dans champ de vision
+		if (bases == null | bases.size() == 0)
+		{
+			WarMessage m = getMessageFromBase();
+			
+			if (m != null)
+				explorer.getBrain().setHeading(m.getAngle());
+			
+			// J'envoie un message aux bases pour savoir où elles sont
+			explorer.getBrain().broadcastMessageToAgentType(WarAgentType.WarBase, Constants.whereAreYou, (String[]) null);
+			
+			explorer.setToReturn(MovableWarAgent.ACTION_MOVE);
+		}
+		else		// On voit une base
+		{
+			WarPercept base = bases.get(0);
+			
+			// Si la base est assez proche pour qu'on lui donne
+			if (base.getDistance() <= MovableWarAgent.MAX_DISTANCE_GIVE) {
+				explorer.setTacheCourante(new DonnerNourritureBase(typeAgent));
+				return;
+			}
+			else {		// Si elle est trop loin, on se dirige vers elle
+				explorer.getBrain().setHeading(base.getAngle());
+				explorer.setToReturn(MovableWarAgent.ACTION_MOVE);
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Tache Retourner Nourriture";
+	}
+}
