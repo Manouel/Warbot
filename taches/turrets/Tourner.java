df7a09b0f814965f147b14da2e27b1f1177ebbdc
Gouga34 <imala34@hotmail.fr>
2014-12-11 12:13:34 +0100

    Tourelles tournent

---
+++ taches/turrets/Tourner.java
@@ -0,0 +1,36 @@
+package pepisha.taches.turrets;
+
+import java.util.ArrayList;
+
+import pepisha.WarTurretBrainController;
+import pepisha.taches.TacheAgent;
+import edu.turtlekit3.warbot.agents.percepts.WarPercept;
+import edu.turtlekit3.warbot.brains.WarBrainController;
+
+public class Tourner extends TacheAgent {
+
+	public Tourner(WarBrainController b) {
+		super(b);
+	}
+
+	@Override
+	public void exec() {
+		WarTurretBrainController turret = (WarTurretBrainController) typeAgent;
+		
+		ArrayList<WarPercept> percept = turret.getBrain().getPerceptsEnemies();
+
+		if(percept != null && percept.size() > 0){	
+			Attaquer nvTache=new Attaquer(turret);
+			turret.setTacheCourante(nvTache);
+		}
+		else{
+			turret.getBrain().setHeading(turret.getBrain().getHeading()+180);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Tache Tourner";
+	}
+	
+}
